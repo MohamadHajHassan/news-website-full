62f8d1302db6e9ffc4c563a263d8eadbd8a3bd32
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-23 15:55:26 +0300

    add get news api

---
+++ backend/get_news.php
@@ -0,0 +1,16 @@
+<?php
+header("Access-Control-Allow-origin:*");
+include("connection.php");
+
+$query = $mysqli->prepare("SELECT title, descript FROM news");
+$query->execute();
+$array = $query->get_result();
+
+$response = [];
+
+while($m = $array->fetch_assoc()){
+    $response[] = $m;
+}
+
+echo json_encode($response);
+?>
